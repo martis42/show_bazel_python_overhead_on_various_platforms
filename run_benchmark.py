16388f07b03c2fc353b8e546e062b6eb307c91a1
Martin Medler <martis42.dev@gmail.com>
2024-03-10 19:45:20 +0100

    Fix and improve tests
    - Fix usage of startup options
    - Drop retesting as it has no valuable information
    - Restructure code in preparation for potentially running multiple setups
      in a single CI job

--- run_benchmark.py
+++ run_benchmark.py
@@ -6,56 +6,8 @@ import math
 import re
 import subprocess
 import sys
-from dataclasses import dataclass
 
 BENCHMARK_RUNS = 5
-
-
-@dataclass
-class BenchmarkResult:
-    clean_time: float
-    retest_time: float
-
-
-def extract_benchmark_time(process: subprocess.CompletedProcess) -> float:
-    elapsed_time_match = re.findall(r"Elapsed time: ([0-9.]+)s, Critical Path: [0-9.]+s", process.stderr)
-    if not elapsed_time_match or len(elapsed_time_match) > 1:
-        print(f"ERROR: Expected result for elapsed time '{elapsed_time_match}'")
-        sys.exit(1)
-    return float(elapsed_time_match[0])
-
-
-def run_benchmark_step(workspace: str, extra_args: list[str], is_retest: bool) -> float:
-    retest_arg = ["--nocache_test_results"] if is_retest else []
-    cmd = ["bazel", "test"] + retest_arg + extra_args + ["//..."]
-    process = subprocess.run(cmd, cwd=workspace, check=False, capture_output=True, text=True)
-    if process.returncode != 0:
-        print(f"ERROR: Command failed '{cmd}'")
-        print("===  stdout  ===")
-        print(process.stdout)
-        print("================")
-        print("===  stderr  ===")
-        print(process.stderr)
-        print("================")
-        sys.exit(1)
-    return extract_benchmark_time(process)
-
-
-def run_benchmark(workspace: str, extra_args: list[str]) -> BenchmarkResult:
-    print(">>> Ensure all external deps are cached locally and show Python version\n")
-    setup_cmd = ["bazel", "test"] + extra_args + ["some_test_0"]
-    subprocess.run(setup_cmd, cwd=workspace, check=True)
-
-    print("\n>>> Ensure no cached results or runfile trees exist")
-    subprocess.run(["bazel", "clean"], cwd=workspace, check=True, capture_output=True)
-
-    print("\n>>> Run benchmark\n")
-    clean_time = run_benchmark_step(workspace=workspace, extra_args=extra_args, is_retest=False)
-    print(f"Clean time: {clean_time}")
-    retest_time = run_benchmark_step(workspace=workspace, extra_args=extra_args, is_retest=False)
-    print(f"Retest time: {retest_time}")
-
-    return BenchmarkResult(clean_time=clean_time,retest_time=retest_time)
 
 
 def mean_and_std_dev(values: list[float]) -> (float, float):
@@ -69,18 +21,45 @@ def mean_and_std_dev(values: list[float]) -> (float, float):
     return mean, std_dev
 
 
-def analyze_results(values: list[BenchmarkResult]):
-    clean_mean, clean_std_dev = mean_and_std_dev([result.clean_time for result in values])
-    retest_mean, retest_std_dev = mean_and_std_dev([result.retest_time for result in values])
-
-    print("\nBenchmark results for clean run:")
+def analyze_results(values: list[float]):
+    clean_mean, clean_std_dev = mean_and_std_dev(values)
+    print("\nBenchmark results:")
     print(f"  Mean: {clean_mean:.3f} s, standard deviation {clean_std_dev:.3f} s")
-    print("Benchmark results for retest:")
-    print(f"  Mean: {retest_mean:.3f} s, standard deviation {retest_std_dev:.3f} s")
 
 
-def main(workspace: str, extra_args: list[str]):
-    benchmark_times = [run_benchmark(workspace=workspace, extra_args=extra_args) for _ in range(0, BENCHMARK_RUNS)]
+def extract_benchmark_time(process: subprocess.CompletedProcess) -> float:
+    elapsed_time_match = re.findall(r"Elapsed time: ([0-9.]+)s, Critical Path: [0-9.]+s", process.stderr)
+    if not elapsed_time_match or len(elapsed_time_match) > 1:
+        print(f"ERROR: Expected result for elapsed time '{elapsed_time_match}'")
+        sys.exit(1)
+    return float(elapsed_time_match[0])
+
+
+def run_benchmark_step(workspace: str, clean_cmd: list[str], benchmark_cmd: list[str]) -> float:
+    print("\n>>> Ensure no cached results or runfile trees exist")
+    subprocess.run(clean_cmd, cwd=workspace, check=True, capture_output=True)
+
+    print("\n>>> Run benchmark")
+    process = subprocess.run(benchmark_cmd, cwd=workspace, check=True, capture_output=True, text=True)
+    benchmark_time = extract_benchmark_time(process)
+    print(f"Elapsed time: {benchmark_time}")
+
+    return benchmark_time
+
+
+def run_benchmark(workspace: str, extra_args: list[str], extra_startup_args: list[str]) -> None:
+    base_cmd = ["bazel"] + extra_startup_args + ["test"] + extra_args
+
+    print(">>> Ensure all external deps are cached locally and show Python version\n")
+    setup_cmd = base_cmd + ["some_test_0"]
+    subprocess.run(setup_cmd, cwd=workspace, check=True)
+
+    benchmark_times = [run_benchmark_step(
+        workspace=workspace,
+        clean_cmd=["bazel"] + extra_startup_args + ["clean"],
+        benchmark_cmd=base_cmd + ["//..."])
+        for _ in range(0, BENCHMARK_RUNS)]
+
     analyze_results(benchmark_times)
 
 
@@ -97,7 +76,15 @@ if __name__ == "__main__":
         nargs='+',
         help="Further arguments for the benchmarked Bazel commands. Provide arguments without leading '--'"
     )
+    parser.add_argument(
+        '--extra_startup_args',
+        type=str,
+        nargs='+',
+        help="Further startup arguments for the benchmarked Bazel commands. Provide arguments without leading '--'"
+    )
     args = parser.parse_args()
 
     bazel_extra_args = [f"--{bazel_arg}" for bazel_arg in args.extra_args] if args.extra_args else []
-    main(workspace=args.workspace, extra_args=bazel_extra_args)
+    bazel_extra_startup_args = [f"--{bazel_arg}" for bazel_arg in args.extra_args] if args.extra_args else []
+
+    run_benchmark(workspace=args.workspace, extra_args=bazel_extra_args, extra_startup_args=bazel_extra_startup_args)
